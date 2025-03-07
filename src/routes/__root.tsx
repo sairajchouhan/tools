38c98b4f5946b3fba9f2609ff163ba75a91b5dfc
Sairaj Chouhan <sairaj2119@gmail.com>
2025-03-07 14:53:34 +0530

    devtools in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,25 @@
-import { createRootRoute,  Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Suspense, lazy } from "react";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
-})
+});
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
