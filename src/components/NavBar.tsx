429c5564f0a1120357b2eaba8cd690a129b56f24
Sairaj Chouhan <sairaj2119@gmail.com>
2025-03-29 16:15:06 +0530

    about:

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,15 +28,6 @@ export function NavBar() {
               >
                 JSON Diff
               </Link>
-              <Link 
-                to="/about" 
-                className="text-gray-700 hover:text-gray-900 hover:bg-gray-100 rounded-md px-3 py-2 text-sm font-medium"
-                activeProps={{
-                  className: "bg-gray-100 text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
-                }}
-              >
-                About
-              </Link>
             </div>
           </div>
         </div>
